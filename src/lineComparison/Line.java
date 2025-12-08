4466702874381b9f07be3c3a725f28aadaef7cc5
vedu2811 <vedant28j@gmail.com>
2025-12-08 17:25:57 +0530

    UC - 3 Done

--- src/lineComparison/Line.java
+++ src/lineComparison/Line.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Line {
     Point p1;
     Point p2;
+    double length;
 
     Line(Point p1,Point p2){
         this.p1=p1;
@@ -12,7 +13,8 @@ public class Line {
     }
 
     public void calculateLength(){
-        double length=(Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2)));
-        System.out.println("Length: "+length);
+        double l=(Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2)));
+        System.out.println("Length: "+l);
+        length=l;
     }
 }
--- src/lineComparison/Main.java
+++ src/lineComparison/Main.java
@@ -6,6 +6,15 @@ public class Main {
         Main n1=new Main();
         Line l1=new Line(n1.getPoint(),n1.getPoint());
         l1.calculateLength();
+        Line l2=new Line(n1.getPoint(),n1.getPoint());
+        l2.calculateLength();
+        if(l1.length>l2.length){
+            System.out.println("Line 1 is larger");
+        }else if(l1.length<l2.length){
+            System.out.println("Line 2 is larger");
+        }else{
+            System.out.println("Equal length of both lines");
+        }
     }
     public Point getPoint(){
         Scanner sc=new Scanner(System.in);
