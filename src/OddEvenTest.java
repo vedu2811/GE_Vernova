28077e44f4444affca10c11943d10ea2b6f04c15
vedu2811 <vedant28j@gmail.com>
2025-12-08 13:00:51 +0530

    Check whether number is odd or even

---
+++ src/OddEvenTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class OddEvenTest {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num<0){
+            System.out.println("Negative");
+        }
+        if(num%2==0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is Odd");
+        }
+    }
+}
