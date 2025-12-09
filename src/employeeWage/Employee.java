b1d8e898d0b0db5c262ce05261771c0802836179
vedu2811 <vedant28j@gmail.com>
2025-12-09 15:03:18 +0530

    Added : UC2 - daily wage method

--- src/employeeWage/Employee.java
+++ src/employeeWage/Employee.java
@@ -2,10 +2,10 @@ package employeeWage;
 
 public class Employee {
     int dailyAttendance;
-//    int workPerDay=8;
+    int workPerDay=8;
 //    int halfDay=4;
-//    int hourlyPay=20;
-//    int dailySalary = 0;
+    int hourlyPay=20;
+    int dailySalary = 0;
     public void calculateAttendance(){
         if(Math.random()<0.5){
             dailyAttendance=0;
@@ -15,15 +15,12 @@ public class Employee {
             System.out.println("Present");
         }
     }
-//    public void dailyWage(){
-//        if(dailyAttendance==1){
-//            dailySalary=halfDay*hourlyPay;
-//            System.out.println(dailySalary);
-//        }else if(dailyAttendance==2){
-//            dailySalary=workPerDay*hourlyPay;
-//            System.out.println(dailySalary);
-//        }else{
-//            System.out.println("No Pay");
-//        }
-//    }
+    public void dailyWage(){
+        if(dailyAttendance==1){
+            dailySalary=workPerDay*hourlyPay;
+            System.out.println(dailySalary);
+        }else{
+            System.out.println("No Pay");
+        }
+    }
 }
--- src/employeeWage/Main.java
+++ src/employeeWage/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String args[]){
         Employee e1 = new Employee();
         e1.calculateAttendance();
-//        e1.dailyWage();
+        e1.dailyWage();
     }
 }
