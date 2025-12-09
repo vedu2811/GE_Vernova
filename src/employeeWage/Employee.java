f5511a0d8a3b9a39c1f4b6b22ce4687929b57481
vedu2811 <vedant28j@gmail.com>
2025-12-09 15:10:47 +0530

    Added : UC3 - Part time wage code

--- src/employeeWage/Employee.java
+++ src/employeeWage/Employee.java
@@ -3,20 +3,26 @@ package employeeWage;
 public class Employee {
     int dailyAttendance;
     int workPerDay=8;
-//    int halfDay=4;
+    int halfDay=4;
     int hourlyPay=20;
     int dailySalary = 0;
     public void calculateAttendance(){
-        if(Math.random()<0.5){
+        if(((Math.random()*100)%3)==0){
             dailyAttendance=0;
             System.out.println("Absent");
-        }else{
+        }else if(((Math.random()*100)%3)==1){
             dailyAttendance=1;
+            System.out.println("Part Time");
+        }else{
+            dailyAttendance=2;
             System.out.println("Present");
         }
     }
     public void dailyWage(){
         if(dailyAttendance==1){
+            dailySalary=halfDay*hourlyPay;
+            System.out.println(dailySalary);
+        }else if(dailyAttendance==2){
             dailySalary=workPerDay*hourlyPay;
             System.out.println(dailySalary);
         }else{
