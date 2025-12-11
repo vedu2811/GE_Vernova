601e295d478b1ae373690ccb9b89bc098ffaee10
vedu2811 <vedant28j@gmail.com>
2025-12-11 11:51:25 +0530

    Starting UC8

--- src/employeeWage/Company.java
+++ src/employeeWage/Company.java
@@ -1,4 +1,31 @@
 package employeeWage;
 
 public class Company {
+    int companyWage;
+    int companyWorkingHours;
+    int companyWorkingDays;
+
+    public int getCompanyWage() {
+        return companyWage;
+    }
+
+    public void setCompanyWage(int companyWage) {
+        this.companyWage = companyWage;
+    }
+
+    public int getCompanyWorkingHours() {
+        return companyWorkingHours;
+    }
+
+    public void setCompanyWorkingHours(int companyWorkingHours) {
+        this.companyWorkingHours = companyWorkingHours;
+    }
+
+    public int getCompanyWorkingDays() {
+        return companyWorkingDays;
+    }
+
+    public void setCompanyWorkingDays(int companyWorkingDays) {
+        this.companyWorkingDays = companyWorkingDays;
+    }
 }
